f475cbe5e37bd82ee332546a4bd4f522e178121a
sidneyvmml <sidneyvictormml@gmail.com>
2021-05-06 20:36:26 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/devsuperior/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.devsuperior.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.devsuperior.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
